d570036a77909f18ee122674c6c70e65c80ef37d
Ilya Kumpan <Kumpan2003@yandex.ru>
2025-01-13 10:13:40 +0300

    add and remove admin(barista)

--- backend/handlers.py
+++ backend/handlers.py
@@ -3,14 +3,16 @@ from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
-from queries import add_user, get_buy_coffee, get_user_by_phone, add_coffee
+import queries as rq
+import keyboards as kb
+
 from filters import RegistrationFilter, IsadminFilter, IsSuperUserFilter
 from middleware import RegistrationMiddleware
-import keyboards as kb
 
 
 router = Router()
 router.message.middleware(RegistrationMiddleware())
+
 
 # Приветственные хендлеры для Суперпользователя(владельца), Админа(баристы)
 # Авторизованных/неавторизованных пользователей
@@ -64,7 +66,7 @@ async def registration(msg: types.Message):
                 "Некорректный номер телефона. Попробуйте снова или обратитесь к Баристе."
             )
             return
-        await add_user(msg.chat.id, formatted_number)
+        await rq.add_user(msg.chat.id, formatted_number)
         await msg.answer(
             f"<b>Sakura Michi приветствует Вас, {msg.from_user.first_name}&#129303;\n\nМы очень стараемся для, Вас, постоянно совершенствуя рецепты, согласно Японским традициям.\nЧтобы ваши вкусовые сосочки сказали нам СПАСИБО!\n\nВ данный момент у нас проходит АКЦИЯ&#9749;\nКупи 5 больших стаканов любого кофе и получи 6-ой В ПОДАРОК &#127873;</b>",
             reply_markup=kb.menu_user,
@@ -74,13 +76,13 @@ async def registration(msg: types.Message):
         await msg.answer("Это не ваш контакт! Пожалуйста, введите СВОЙ номер телефона.")
 
 
-# Хендлер для зарегистрированных пользователей на кнопку профиля
+# Хендлер на кнопку профиля для зарегистрированных пользователей
 
 
 @router.message(F.text == "Мой профиль")
 async def my_profile(msg: types.Message):
     tg_id = msg.from_user.id
-    buy_coffee = await get_buy_coffee(tg_id)
+    buy_coffee = await rq.get_buy_coffee(tg_id)
     if buy_coffee < 5:
         bonus_coffee = 5 - buy_coffee
         await msg.answer(
@@ -95,7 +97,7 @@ async def my_profile(msg: types.Message):
 # Добавление кофе пользователю
 
 
-class AddCoffeeState(StatesGroup):
+class CoffeeState(StatesGroup):
     waiting_for_phone = State()
     waiting_for_buy_coffe = State()
 
@@ -107,12 +109,12 @@ class AddCoffeeState(StatesGroup):
 )
 async def start_add_coffee(message: types.Message, state: FSMContext):
     await message.answer(
-        "Введите номер телефона пользователя начиная с 8:\nПример: 89012345678"
+        "Для ДОБАВЛЕНИЯ КОФЕ пользователю:\nВведите номер телефона пользователя начиная с 8!\n(Пример: 89012345678)"
     )
-    await state.set_state(AddCoffeeState.waiting_for_phone)
+    await state.set_state(CoffeeState.waiting_for_phone)
 
 
-@router.message(AddCoffeeState.waiting_for_phone)
+@router.message(CoffeeState.waiting_for_phone)
 async def process_phone(message: types.Message, state: FSMContext):
     phone = message.text.strip()
 
@@ -124,7 +126,7 @@ async def process_phone(message: types.Message, state: FSMContext):
 
     normalized_phone = phone[1:]
 
-    user = await get_user_by_phone(normalized_phone)
+    user = await rq.get_user_by_phone(normalized_phone)
     if not user:
         await message.answer(
             f"Пользователь с номером {phone} не найден.\nПроверьте правильность введенного номера и повторите попытку!"
@@ -134,10 +136,10 @@ async def process_phone(message: types.Message, state: FSMContext):
     await state.update_data(phone=normalized_phone)
 
     await message.answer("Введите количество купленных кофе:")
-    await state.set_state(AddCoffeeState.waiting_for_buy_coffe)
+    await state.set_state(CoffeeState.waiting_for_buy_coffe)
 
 
-@router.message(AddCoffeeState.waiting_for_buy_coffe)
+@router.message(CoffeeState.waiting_for_buy_coffe)
 async def process_coffee_count(message: types.Message, state: FSMContext):
     try:
         buy_coffe = int(message.text.strip())
@@ -150,12 +152,21 @@ async def process_coffee_count(message: types.Message, state: FSMContext):
     user_data = await state.get_data()
     phone = user_data.get("phone")
 
-    success = await add_coffee(phone, buy_coffe)
+    success = await rq.add_coffee(phone, buy_coffe)
 
     if success:
         await message.answer(
             f"Добавлено {buy_coffe} кофе для пользователя с номером {phone}."
         )
+        user = await rq.get_user_by_phone(phone)
+        if user and user.tg_id:
+            try:
+                await message.bot.send_message(
+                    user.tg_id,
+                    f"<b>Вам начислено {buy_coffe} стакан(а) кофе.\nВаш текущий баланс: {user.buy_coffe} стакан(а).</b>",
+                )
+            except Exception as e:
+                await message.answer(f"Клиенту не удалось отправить уведомление: {e}")
     else:
         await message.answer(
             f"Произошла ошибка при добавлении кофе для пользователя с номером {phone}."
@@ -163,11 +174,276 @@ async def process_coffee_count(message: types.Message, state: FSMContext):
     await state.clear()
 
 
-# Ответ ботом на любое из сообщений кроме кнопок
+# Выдать подарочный кофе(тем самым списываем 5 стаканов со счета клиента)
+
+
+class FreeCoffeeStates(StatesGroup):
+    waiting_for_phone = State()
+    confirming_usage = State()
+
+
+@router.message(
+    F.text == "Выдать Бесплатный Кофе",
+    RegistrationFilter(),
+    IsadminFilter(),
+)
+async def start_used_coffee(message: types.Message, state: FSMContext):
+    await message.answer(
+        "Для выдачи БЕСПЛАТНОГО КОФЕ:\nВедите номер телефона пользователя начиная с 8!\n(Пример: 89012345678)"
+    )
+    await state.set_state(FreeCoffeeStates.waiting_for_phone)
+
+
+@router.message(FreeCoffeeStates.waiting_for_phone)
+async def process_phone_check(message: types.Message, state: FSMContext):
+    phone = message.text.strip()
+
+    if not phone.startswith("8") or len(phone) != 11 or not phone.isdigit():
+        await message.answer(
+            "Введите корректный номер телефона, начинающийся с 8 и длиной 11 цифр.\n(Пример: 89012345678)"
+        )
+        return
+
+    normalized_phone = phone[1:]
+
+    try:
+        user = await rq.get_user_by_phone(normalized_phone)
+    except Exception as e:
+        await message.answer(f"Ошибка при запросе пользователя: {e}")
+        return
+
+    if not user:
+        await message.answer(
+            f"Пользователь с номером {phone} не найден.\nПроверьте правильность введенного номера и повторите попытку!"
+        )
+        return
+
+    await state.update_data(phone=normalized_phone)
+    await message.answer(
+        f"Ты уверен, что хочешь выдать БЕСПЛАТНЫЙ КОФЕ пользователю: {phone}?\n\nОтправь любое слово - и я сразу выдам!\n\nЕсли ты ошибся нажми на кнопку выдачи и начни сначала!"
+    )
+
+    await state.set_state(FreeCoffeeStates.confirming_usage)
+
+
+@router.message(FreeCoffeeStates.confirming_usage)
+async def process_coffee_used(message: types.Message, state: FSMContext):
+    user_data = await state.get_data()
+    phone = user_data.get("phone")
+
+    user = await rq.get_user_by_phone(phone)
+    if not user:
+        await message.answer("Пользователь не найден.")
+        await state.clear()
+        return
+
+    if user.buy_coffe < 5:
+        await message.answer(
+            f"Недостаточно кофе для выдачи бесплатного стакана. "
+            f"На счету пользователя: {user.buy_coffe} стаканов."
+        )
+        await state.clear()
+        return
+
+    success = await rq.used_free_coffe(phone)
+
+    if success:
+        await message.answer("Выдаю бесплатный кофе...")
+        await message.answer(
+            f"Выдан бесплатный кофе для пользователя с номером {phone}. "
+            f"Оставшееся количество: {user.buy_coffe - 5}"
+        )
+        try:
+            await message.bot.send_message(
+                user.tg_id,
+                f"<b>Поздравляем, Вы получили БЕСПЛАТНЫЙ КОФЕ&#127873;\nВаш текущий баланс: {user.buy_coffe - 5} стакана(ов).</b>",
+            )
+        except Exception as e:
+            await message.answer(f"Клиенту не удалось отправить уведомление: {e}")
+    else:
+        await message.answer(
+            f"Произошла ошибка при выдаче кофе для пользователя с номером {phone}."
+        )
+    await state.clear()
+
+
+# Добавление Баристы(админа) Суперпользователем(владельцем)
+
+
+class AddIsAdminState(StatesGroup):
+    waiting_for_phone_admin = State()
+    admin_flag = State()
+
+
+@router.message(F.text == "Добавить Баристу", RegistrationFilter(), IsSuperUserFilter())
+async def start_admin_add(message: types.Message, state: FSMContext):
+    await message.answer(
+        "Чтобы ДОБАВИТЬ БАРИСТУ:\nВведите номер телефона пользователя начиная с 8!\n(Пример: 89012345678)"
+    )
+    await state.set_state(AddIsAdminState.waiting_for_phone_admin)
+
+
+@router.message(AddIsAdminState.waiting_for_phone_admin)
+async def check_phone(message: types.Message, state: FSMContext):
+    phone = message.text.strip()
+
+    if not phone.startswith("8") or len(phone) != 11 or not phone.isdigit():
+        await message.answer(
+            "Введите корректный номер телефона, начинающийся с 8 и длиной 11 цифр.\n(Пример: 89012345678)"
+        )
+        return
+
+    normalized_phone = phone[1:]
+
+    try:
+        user = await rq.get_user_by_phone(normalized_phone)
+    except Exception as e:
+        await message.answer(f"Ошибка при запросе пользователя: {e}")
+        return
+
+    if not user:
+        await message.answer(
+            f"Пользователь с номером {phone} не найден.\nПроверьте правильность введенного номера и повторите попытку!"
+        )
+        return
+
+    await state.update_data(phone=normalized_phone)
+    await message.answer(
+        f"Ты уверен, что хочешь НАЗНАЧИТЬ на должность БАРИСТЫ пользователя: {phone}?\n\nОтправь любое слово - и я сразу назначу его!\n\nЕсли ты ошибся нажми на кнопку добавления и начни сначала!"
+    )
+
+    await state.set_state(AddIsAdminState.admin_flag)
+
+
+@router.message(AddIsAdminState.admin_flag)
+async def add_admin(message: types.Message, state: FSMContext):
+    user_data = await state.get_data()
+    phone = user_data.get("phone")
+
+    user = await rq.get_user_by_phone(phone)
+    if not user:
+        await message.answer("Пользователь не найден.")
+        await state.clear()
+        return
+
+    if user.is_admin:
+        await message.answer("Этот пользователь уже бариста")
+        await state.clear()
+        return
+
+    success = await rq.add_admin(phone)
+
+    if success:
+        await message.answer(
+            f"Пользователь: {phone} успешно стал баристой, мои соболезнования ему..."
+        )
+        user = await rq.get_user_by_phone(phone)
+        chat = await message.bot.get_chat(user.tg_id)
+        user_first_name = chat.first_name
+        if user and user.tg_id:
+            try:
+                await message.bot.send_message(
+                    user.tg_id,
+                    f"Добро пожаловать в команду Sakura Michi, {user_first_name}\nПоздравляю тебя с должностью: Бариста!",
+                )
+            except Exception as e:
+                await message.answer(f"Баристе не удалось отправить уведомление: {e}")
+    else:
+        await message.answer("Произошла ошибка при добавлении Баристы")
+
+    await state.clear()
+
+
+# Удаление Баристы(админа) Суперпользователем(владельцем)
+
+
+class RemoveIsAdminState(StatesGroup):
+    waiting_for_phone_admin = State()
+    admin_flag = State()
+
+
+@router.message(F.text == "Удалить Баристу", RegistrationFilter(), IsSuperUserFilter())
+async def start_admin_remove(message: types.Message, state: FSMContext):
+    await message.answer(
+        "Чтобы УДАЛИТЬ БАРИСТУ:\nВведите номер телефона пользователя начиная с 8!\n(Пример: 89012345678)"
+    )
+    await state.set_state(RemoveIsAdminState.waiting_for_phone_admin)
+
+
+@router.message(RemoveIsAdminState.waiting_for_phone_admin)
+async def check_phone_admin(message: types.Message, state: FSMContext):
+    phone = message.text.strip()
+
+    if not phone.startswith("8") or len(phone) != 11 or not phone.isdigit():
+        await message.answer(
+            "Введите корректный номер телефона, начинающийся с 8 и длиной 11 цифр.\n(Пример: 89012345678)"
+        )
+        return
+
+    normalized_phone = phone[1:]
+
+    try:
+        user = await rq.get_user_by_phone(normalized_phone)
+    except Exception as e:
+        await message.answer(f"Ошибка при запросе пользователя: {e}")
+        return
+
+    if not user:
+        await message.answer(
+            f"Пользователь с номером {phone} не найден.\nПроверьте правильность введенного номера и повторите попытку!"
+        )
+        return
+
+    await state.update_data(phone=normalized_phone)
+    await message.answer(
+        f"Ты уверен, что хочешь Уволить пользователя: {phone}?\n\nОтправь любое слово - и я сразу уволю его!\n\nЕсли ты ошибся нажми на кнопку добавления и начни сначала!"
+    )
+
+    await state.set_state(RemoveIsAdminState.admin_flag)
+
+
+@router.message(RemoveIsAdminState.admin_flag)
+async def remove_admin(message: types.Message, state: FSMContext):
+    user_data = await state.get_data()
+    phone = user_data.get("phone")
+
+    user = await rq.get_user_by_phone(phone)
+    if not user:
+        await message.answer("Пользователь не найден.")
+        await state.clear()
+        return
+
+    if user.is_admin is False:
+        await message.answer("Этот пользователь не бариста")
+        await state.clear()
+        return
+
+    success = await rq.remove_admin(phone)
+
+    if success:
+        await message.answer(f"Пользователь: {phone}, успешно уволен!")
+        user = await rq.get_user_by_phone(phone)
+        chat = await message.bot.get_chat(user.tg_id)
+        user_first_name = chat.first_name
+        if user and user.tg_id:
+            try:
+                await message.bot.send_message(
+                    user.tg_id,
+                    f"Спасибо за время проведенное вместе, {user_first_name}. Мы желаем тебе успехов в дальнейшей карьере!\nC уважением команда Sakura Michi",
+                )
+            except Exception as e:
+                await message.answer(f"Баристе не удалось отправить уведомление: {e}")
+    else:
+        await message.answer("Произошла ошибка при добавлении Баристы")
+
+    await state.clear()
+
+
+# Ответ ботом на любое сообщение (кроме команд)
 
 
 @router.message()
 async def non_command(msg: types.Message):
     await msg.answer(
-        "<b>Уважаемый Клиент!\nЯ не понимаю сообщения подобного характера, воспользуйся кнопкой&#129325;</b>"
+        "<b>Уважаемый Клиент!\nЯ не понимаю сообщения подобного характера, воспользуйтесь кнопкой&#129325;</b>"
     )
--- backend/queries.py
+++ backend/queries.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from database import async_session
 from models import User
 from sqlalchemy import select
@@ -28,27 +29,9 @@ async def get_user_by_phone(phone: str) -> User | None:
         return user
 
 
-# async def add_coffee(phone: str, coffee_count: int, session: AsyncSession) -> bool:
-#     try:
-#         stmt = select(User).filter(User.phone == phone)
-#         result = await session.execute(stmt)
-#         user = result.scalar_one_or_none()
-
-
-#         if not user:
-#             return False
-#         user.buy_coffe += coffee_count
-#         await session.commit()
-#         return True
-#     except Exception as e:
-#         print(f"Ошибка при добавлении кофе: {e}")
-#         await session.rollback()
-#         return False
 async def add_coffee(phone: str, coffee_count: int) -> bool:
     try:
-        # Создаём сессию внутри функции
         async with async_session() as session:
-            # Находим пользователя по номеру телефона
             user_phone = select(User).filter(User.phone == phone)
             result = await session.execute(user_phone)
             user = result.scalar_one_or_none()
@@ -56,12 +39,57 @@ async def add_coffee(phone: str, coffee_count: int) -> bool:
             if not user:
                 return False
 
-            # Добавляем кофе
             user.buy_coffe += coffee_count
-
-            # Сохраняем изменения
             await session.commit()
             return True
     except Exception as e:
         print(f"Ошибка при добавлении кофе: {e}")
         return False
+
+
+async def used_free_coffe(phone: str) -> bool:
+    try:
+        async with async_session() as session:
+            user_phone = select(User).filter(User.phone == phone)
+            result = await session.execute(user_phone)
+            user = result.scalar_one_or_none()
+
+            if not user:
+                return False
+
+            user.buy_coffe -= 5
+            await session.commit()
+            return True
+    except Exception as e:
+        print(f"Ошибка при списании кофе: {e}")
+        return False
+
+
+async def add_admin(phone: str) -> Optional[User]:
+    async with async_session() as session:
+        user_phone = select(User).filter(User.phone == phone)
+        result = await session.execute(user_phone)
+        user = result.scalar_one_or_none()
+
+        if not user:
+            return None
+
+        user.is_admin = True
+        await session.commit()
+        session.expunge(user)
+        return user
+
+
+async def remove_admin(phone: str) -> Optional[User]:
+    async with async_session() as session:
+        user_phone = select(User).filter(User.phone == phone)
+        result = await session.execute(user_phone)
+        user = result.scalar_one_or_none()
+
+        if not user:
+            return None
+
+        user.is_admin = False
+        await session.commit()
+        session.expunge(user)
+        return user
